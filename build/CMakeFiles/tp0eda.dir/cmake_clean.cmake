c1d10eb29ddfcbe3c6f933c95ce020f121a273f7
DinosaurioBlue <franco21bonacina07@gmail.com>
2024-09-01 17:28:46 -0300

    commititng

---
+++ build/CMakeFiles/tp0eda.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tp0eda.dir/extra.cpp.o"
+  "CMakeFiles/tp0eda.dir/extra.cpp.o.d"
+  "CMakeFiles/tp0eda.dir/main.cpp.o"
+  "CMakeFiles/tp0eda.dir/main.cpp.o.d"
+  "tp0eda"
+  "tp0eda.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tp0eda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
