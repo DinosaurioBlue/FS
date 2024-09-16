b71ff2f5c947d4041f424dccd0ec9536e0ddcbef
DinosaurioBlue <franco21bonacina07@gmail.com>
2024-09-16 18:04:10 -0300

    changes

--- extra.h
+++ extra.h
@@ -1,1 +1,19 @@
 
+#include <iostream>
+#include<string>
+
+
+using namespace std;
+
+
+class BankAccount{
+    public:
+    string name;
+    int balance;
+    void withdraw (int amount){
+        balance -= amount;
+    }
+    private:
+    
+
+};
